0628a977019d27d2544766a1d324811cd352f277
Jan Koeppen <jan@yaneq.com>
2024-05-03 18:44:38 -0300

    cron fix

--- chatbot/trigger_interaction.py
+++ chatbot/trigger_interaction.py
@@ -1,9 +1,6 @@
 from lib.config import Config
 from lib.env import env
-from lib.assistant import summarize_week
-from asgiref.sync import sync_to_async
 from telegram.ext import ApplicationBuilder
-from datetime import datetime, timedelta
 import db
 from diary.models import User
 from lib.threads import send_message_to_assistant
@@ -12,17 +9,14 @@ config = Config.load()
 
 
 async def trigger_interaction(user, bot, prompt):
-    reply = await send_message_to_assistant(
-        user, prompt, config.assistant_id_life_coach
-    )
-    await bot.send_message(
+    reply = await send_message_to_assistant(user, prompt)
+    telegram_message = await bot.send_message(
         chat_id=user.chat_id,
         text=reply,
     )
 
-    message = await sync_to_async(user.messages.create)(
-        text=reply,
-        author="TherapistBot",
+    await db.persist_message(
+        user, reply, telegram_message.message_id, author="TherapistBot"
     )
 
 
