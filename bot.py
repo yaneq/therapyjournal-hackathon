b8ae9f3c92f093d1a1c0be0043ebb5901370821e
Jan Koeppen <jan@yaneq.com>
2024-05-16 22:30:22 -0300

    update assistant id

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import asyncio
 import sys
 from chatbot.chatbot import serve_bot
 from chatbot.trigger_interaction import trigger_interaction_for_all
-from chatbot.week_in_review import send_week_in_review_to_all
 
 HELP_MESSAGE = "Runs the bot commands\n\
 \n\
@@ -12,9 +11,6 @@ bot.py [command]:\n\
  command:\n\
 \n\
  - serve:               Starts the bot and polls new messages\n\
-- send_week_in_review:      Sends a summary of the week to all user that have\n\
-                            enable_week_in_review flag turned on.\n\
-                            This should be executed by a cron-job.\n\
 - good_morning:         Pings the bot to initiate a conversation\n\
 - good_evening:         Pings the bot to initiate a conversation\n\
 "
@@ -25,8 +21,6 @@ if __name__ == "__main__":
         match command:
             case "serve":
                 serve_bot()
-            case "send_week_in_review":
-                asyncio.run(send_week_in_review_to_all())
             case "good_morning":
                 asyncio.run(trigger_interaction_for_all("start-morning-check-in"))
             case "good_evening":
